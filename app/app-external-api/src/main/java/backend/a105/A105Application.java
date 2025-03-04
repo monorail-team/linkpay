3391b380dcb27fc6835b758cc146f402dc6b660f
wmw04087 <wmw04087@gmail.com>
2025-03-04 14:35:33 +0900

    refactor: 멀티 모듈 구조 변경

---
+++ app/app-external-api/src/main/java/backend/a105/A105Application.java
@@ -0,0 +1,11 @@
+package backend.a105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A105Application {
+    public static void main(String[] args) {
+        SpringApplication.run(A105Application.class, args);
+    }
+}
