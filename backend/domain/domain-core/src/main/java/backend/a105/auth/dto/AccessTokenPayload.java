774375f51da469a144a2d2b23c31dae4c0de02bb
강주빈 <sisoya0424@gmail.com>
2025-03-12 17:55:36 +0900

    chore: todo 제거

--- backend/domain/domain-core/src/main/java/backend/a105/auth/dto/AccessTokenPayload.java
+++ backend/domain/domain-core/src/main/java/backend/a105/auth/dto/AccessTokenPayload.java
@@ -5,7 +5,5 @@ import lombok.Builder;
 import java.util.List;
 
 @Builder
-public record AccessTokenPayload(long memberId,
-                                 List<String> roles) {
-    // todo 할 일 1.패키지 정리, 2.jwt랑 토큰 테스트, 3. SpringSecurity Filter
+public record AccessTokenPayload(long memberId, List<String> roles) {
 }
