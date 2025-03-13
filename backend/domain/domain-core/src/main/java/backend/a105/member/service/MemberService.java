54f9187053272dde3f4533d8b1ed0866635bc019
강주빈 <sisoya0424@gmail.com>
2025-03-13 16:42:13 +0900

    chore: 불필요한 클래스 수정

--- backend/domain/domain-core/src/main/java/backend/a105/member/service/MemberService.java
+++ backend/domain/domain-core/src/main/java/backend/a105/member/service/MemberService.java
@@ -1,7 +1,6 @@
 package backend.a105.member.service;
 
 import backend.a105.member.domain.Member;
-import backend.a105.member.repository.MemberJpaRepository;
 import backend.a105.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +12,8 @@ import java.util.List;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final MemberJpaRepository memberJpaRepository;
 
     public List<Member> getMembers() {
-        return memberJpaRepository.findAll();
+        return memberRepository.findAll();
     }
 }
