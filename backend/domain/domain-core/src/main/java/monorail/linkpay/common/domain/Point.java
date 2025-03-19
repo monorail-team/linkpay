cd8def56bec0581ab02261ae765b8108bbd46b2d
wmw04087 <wmw04087@gmail.com>
2025-03-19 14:10:59 +0900

    feat: point value object 생성

--- backend/domain/domain-core/src/main/java/monorail/linkpay/common/domain/Point.java
+++ backend/domain/domain-core/src/main/java/monorail/linkpay/common/domain/Point.java
@@ -1,34 +1,60 @@
 package monorail.linkpay.common.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import monorail.linkpay.exception.AppException;
 
+import java.util.Objects;
+
+import static lombok.AccessLevel.PROTECTED;
+import static monorail.linkpay.exception.ExceptionCode.INVALID_REQUEST;
+
+@Getter
 @Embeddable
-public record Point(
+@NoArgsConstructor(access = PROTECTED)
+public class Point {
 
-    @Column(nullable = false)
-    long amount
-) {
+    private long amount;
 
-    public Point {
+    public Point(final long amount) {
         if (amount < 0) {
-            throw new IllegalArgumentException("금액은 음수가 될 수 없습니다.");
+            throw new AppException(INVALID_REQUEST, "금액은 음수가 될 수 없습니다.");
         }
+        this.amount = amount;
     }
 
-    public Point add(Point other) {
-        return new Point(this.amount + other.amount);
+    public Point add(final Point point) {
+        return new Point(this.amount + point.amount);
     }
 
-    public Point subtract(Point other) {
-        return new Point(this.amount - other.amount);
+    public Point subtract(final Point point) {
+        return new Point(this.amount - point.amount);
     }
 
-    public Point multiply(long factor) {
-        return new Point(this.amount * factor);
+    public Point multiply(final long value) {
+        if(value < 1) {
+            throw new AppException(INVALID_REQUEST, "곱할 값은 1 이상이어야 합니다.");
+        }
+        return new Point(this.amount * value);
     }
 
-    public Point divide(long factor) {
-        return new Point(this.amount / factor);
+    public Point divide(final long value) {
+        if(value < 1) {
+            throw new AppException(INVALID_REQUEST, "나눌 값은 1 이상이어야 합니다.");
+        }
+        return new Point(this.amount / value);
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object) return true;
+        if (!(object instanceof final Point point)) return false;
+        return amount == point.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
     }
 }
---
+++ backend/domain/domain-core/src/test/java/monorail/linkpay/common/domain/PointTest.java
@@ -0,0 +1,55 @@
+package monorail.linkpay.common.domain;
+
+import monorail.linkpay.exception.AppException;
+import org.junit.jupiter.api.Test;
+
+import static monorail.linkpay.exception.ExceptionCode.INVALID_REQUEST;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+class PointTest {
+
+    private static final Point ONE_THOUSAND = new Point(1000);
+    private static final Point THREE_THOUSAND = new Point(3000);
+    private static final Point FOUR_THOUSAND = new Point(4000);
+
+    @Test
+    void 금액이_음수면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Point(-1))
+            .isInstanceOf(AppException.class)
+            .hasMessage("금액은 음수가 될 수 없습니다.")
+            .extracting("exceptionCode.code")
+            .isEqualTo(INVALID_REQUEST.getCode());
+    }
+
+    @Test
+    void 곱셈_나눗셈_파라미터_검증() {
+        Point point = new Point(1000);
+        assertSoftly(
+            softly -> {
+                softly.assertThatThrownBy(() -> point.multiply(0))
+                    .isInstanceOf(AppException.class)
+                    .hasMessage("곱할 값은 1 이상이어야 합니다.")
+                    .extracting("exceptionCode.code")
+                    .isEqualTo(INVALID_REQUEST.getCode());
+                softly.assertThatThrownBy(() -> point.divide(0))
+                    .isInstanceOf(AppException.class)
+                    .hasMessage("나눌 값은 1 이상이어야 합니다.")
+                    .extracting("exceptionCode.code")
+                    .isEqualTo(INVALID_REQUEST.getCode());
+        });
+    }
+
+    @Test
+    void 금액을_계산한다() {
+        Point point = new Point(2000);
+        assertSoftly(
+            softly -> {
+                softly.assertThat(point.add(ONE_THOUSAND)).isEqualTo(THREE_THOUSAND);
+                softly.assertThat(point.subtract(ONE_THOUSAND)).isEqualTo(ONE_THOUSAND);
+                softly.assertThat(point.divide(2)).isEqualTo(ONE_THOUSAND);
+                softly.assertThat(point.multiply(2)).isEqualTo(FOUR_THOUSAND);
+            }
+        );
+    }
+}
