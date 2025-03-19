1527a878215ebdfb4d604087fa41e0dd7147b582
wmw04087 <wmw04087@gmail.com>
2025-03-20 00:15:10 +0900

    style: 일관된 코드 스타일 유지 (인덴트 및 개행 수정)

--- backend/app/app-external-api/src/main/java/monorail/linkpay/auth/CustomAuthentication.java
+++ backend/app/app-external-api/src/main/java/monorail/linkpay/auth/CustomAuthentication.java
@@ -1,21 +1,19 @@
 package monorail.linkpay.auth;
 
-
+import lombok.Getter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 
+@Getter
 public abstract class CustomAuthentication extends AbstractAuthenticationToken {
+
     private final AuthPrincipal principal;
 
-    public CustomAuthentication(AuthPrincipal principal, Collection<? extends GrantedAuthority> authorities) {
+    protected CustomAuthentication(final AuthPrincipal principal,
+                                   final Collection<? extends GrantedAuthority> authorities) {
         super(authorities);
         this.principal = principal;
-
-    }
-
-    public AuthPrincipal getPrincipal(){
-        return principal;
     }
 }
