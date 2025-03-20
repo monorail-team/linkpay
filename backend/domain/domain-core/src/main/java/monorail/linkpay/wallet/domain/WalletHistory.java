7fb2d1797326789aef149864fe383cb242f6cd86
최성환 <aiya0422@gmail.com>
2025-03-20 16:41:08 +0900

    fix: embedded 중복 해결

--- backend/domain/domain-core/src/main/java/monorail/linkpay/wallet/domain/WalletHistory.java
+++ backend/domain/domain-core/src/main/java/monorail/linkpay/wallet/domain/WalletHistory.java
@@ -24,9 +24,15 @@ public class WalletHistory {
     private Long id;
 
     @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "amount", column = @Column(name = "point"))
+    })
     private Point point;
 
     @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "amount", column = @Column(name = "remaining"))
+    })
     private Point remaining;
 
     @Column(nullable = false, updatable = false)
