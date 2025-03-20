5928cd2fd15519808ca59f43a332172ef81722f2
최성환 <aiya0422@gmail.com>
2025-03-20 09:59:59 +0900

    feat: 지갑 내역 fetcher 추가

--- backend/domain/domain-core/src/main/java/monorail/linkpay/wallet/service/WalletHistoryService.java
+++ backend/domain/domain-core/src/main/java/monorail/linkpay/wallet/service/WalletHistoryService.java
@@ -1,25 +1,19 @@
 package monorail.linkpay.wallet.service;
 
 import lombok.RequiredArgsConstructor;
-import monorail.linkpay.exception.LinkPayException;
 import monorail.linkpay.wallet.domain.WalletHistory;
-import monorail.linkpay.wallet.repository.WalletHistoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import static monorail.linkpay.exception.ExceptionCode.NOT_FOUND_RESOURCE;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class WalletHistoryService {
 
-    private final WalletHistoryRepository walletHistoryRepository;
+    private final WalletHistoryFetcher walletHistoryFetcher;
 
-    public WalletHistoryResponse read(final Long walletHistoryId) {
-        WalletHistory walletHistory = walletHistoryRepository.findById(walletHistoryId)
-            .orElseThrow(() -> new LinkPayException(NOT_FOUND_RESOURCE, "내역 아이디에 해당하는 내역이 존재하지 않습니다."));
-
+    public WalletHistoryResponse read(final Long id) {
+        WalletHistory walletHistory = walletHistoryFetcher.fetchById(id);
         return new WalletHistoryResponse(walletHistory.getId(),
                 walletHistory.getPoint().getAmount(),
                 walletHistory.getTransactionType().toString(),
