37315e7d967ec12b21fe351bb9c0666b29a514ba
강주빈 <sisoya0424@gmail.com>
2025-03-20 16:08:44 +0900

    config: storybook, test, 버전 설정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const content = ['./src/**/*.{js,jsx,ts,tsx}', './.storybook/**/*.{js,ts,jsx,tsx,mdx}'];
 export const theme = {
-  extend: {},
+  extend: {}
 };
 export const plugins = [];
 
