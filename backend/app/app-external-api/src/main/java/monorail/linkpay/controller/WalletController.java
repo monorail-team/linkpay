27cfc1c9bc979ad047d1a2796a4530d8a11ebeaa
최성환 <aiya0422@gmail.com>
2025-03-20 13:34:40 +0900

    feat: 내 지갑 내역 컨트롤러 추가

--- backend/app/app-external-api/src/main/java/monorail/linkpay/controller/WalletController.java
+++ backend/app/app-external-api/src/main/java/monorail/linkpay/controller/WalletController.java
@@ -3,6 +3,8 @@ package monorail.linkpay.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import monorail.linkpay.controller.request.ChargeRequest;
+import monorail.linkpay.wallet.service.WalletHistoryListResponse;
+import monorail.linkpay.wallet.service.WalletHistoryService;
 import monorail.linkpay.wallet.service.WalletResponse;
 import monorail.linkpay.wallet.service.WalletService;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class WalletController {
 
     private final WalletService walletService;
+    private final WalletHistoryService walletHistoryService;
 
     @PatchMapping("/charge")
     public ResponseEntity<Void> chargeWallet(@PathVariable final Long walletId,
@@ -27,5 +30,12 @@ public class WalletController {
     @GetMapping
     public ResponseEntity<WalletResponse> getWallet(@PathVariable final Long walletId) {
         return ResponseEntity.ok(walletService.read(walletId));
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<WalletHistoryListResponse> getWalletHistories(@RequestParam final Long walletId,
+                                                                        @RequestParam final Long lastId,
+                                                                        @RequestParam final int size) {
+        return ResponseEntity.ok(walletHistoryService.readPage(walletId, lastId, size));
     }
 }
---
+++ backend/app/app-external-api/src/main/java/monorail/linkpay/controller/WalletHistoryController.java
@@ -0,0 +1,23 @@
+package monorail.linkpay.controller;
+
+import lombok.RequiredArgsConstructor;
+import monorail.linkpay.wallet.service.WalletHistoryResponse;
+import monorail.linkpay.wallet.service.WalletHistoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/walletHistory")
+public class WalletHistoryController {
+
+    private final WalletHistoryService walletHistoryService;
+
+    @GetMapping("/{walletHistoryId}")
+    public ResponseEntity<WalletHistoryResponse> getWalletHistory(@PathVariable final Long walletHistoryId) {
+        return ResponseEntity.ok(walletHistoryService.read(walletHistoryId));
+    }
+}
