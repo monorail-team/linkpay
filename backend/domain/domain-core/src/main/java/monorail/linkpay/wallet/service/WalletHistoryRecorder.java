9cc357db6518a29b0ce28529f932005617ba05ce
최성환 <aiya0422@gmail.com>
2025-03-20 16:27:21 +0900

    fix: 내 지갑 충전, 차감 서비스 로직 수정

--- backend/domain/domain-core/src/main/java/monorail/linkpay/wallet/service/WalletHistoryRecorder.java
+++ backend/domain/domain-core/src/main/java/monorail/linkpay/wallet/service/WalletHistoryRecorder.java
@@ -4,33 +4,28 @@ import lombok.RequiredArgsConstructor;
 import monorail.linkpay.annotation.SupportLayer;
 import monorail.linkpay.common.domain.Point;
 import monorail.linkpay.common.domain.TransactionType;
-import monorail.linkpay.exception.LinkPayException;
 import monorail.linkpay.util.id.IdGenerator;
 import monorail.linkpay.wallet.domain.Wallet;
 import monorail.linkpay.wallet.domain.WalletHistory;
 import monorail.linkpay.wallet.repository.WalletHistoryRepository;
-import monorail.linkpay.wallet.repository.WalletRepository;
 
 import java.time.LocalDateTime;
-
-import static monorail.linkpay.exception.ExceptionCode.NOT_FOUND_RESOURCE;
 
 @SupportLayer
 @RequiredArgsConstructor
 public class WalletHistoryRecorder {
 
     private final WalletHistoryRepository walletHistoryRepository;
-    private final WalletRepository walletRepository;
     private final IdGenerator idGenerator;
 
-    public Long record(final Long walletId, final Long amount, final TransactionType transactionType) {
-        Wallet wallet = walletRepository.findById(walletId)
-            .orElseThrow(() -> new LinkPayException(NOT_FOUND_RESOURCE, "지갑 아이디에 해당하는 지갑이 존재하지 않습니다."));
-
+    public Long record(final Wallet wallet,
+                       final Long amount,
+                       final Long remaining,
+                       final TransactionType transactionType) {
         return walletHistoryRepository.save(WalletHistory.builder()
             .id(idGenerator.generate())
             .point(new Point(amount))
-            .remaining(new Point(wallet.getAmount()))
+            .remaining(new Point(remaining))
             .wallet(wallet)
             .createdAt(LocalDateTime.now())
             .transactionType(transactionType)
