da2df066454ba4251c6f6f92f0574ea0894ce8d9
진성일 <wls9804@naver.com>
2025-03-24 16:06:53 +0900

    feat: 내 지갑 포인트 조회(백엔드 연결 필요)

--- frontend/src/pages/mywallet/index.tsx
+++ frontend/src/pages/mywallet/index.tsx
@@ -1,12 +1,48 @@
-import React,{useState} from 'react';
+import React,{ useState, useEffect } from 'react';
 import Header from '@/components/Header';
 import ChargeModal from '@/modal/ChargeModal';
 import { MyWalletHistory } from '@/model/MyWalletHistory';
-import { walletData } from '@/mocks/walletData';
+
+
+interface WalletDataResponse {
+  userWalletName: string;
+  amount: number;
+  walletHistory: MyWalletHistory[];
+}
 
 const MyWallet: React.FC = () => {
   const [showChargeModal, setShowChargeModal] = useState(false);
-  const [walletBalance, setWalletBalance] = useState(walletData.availablePoint);
+  const [walletBalance, setWalletBalance] = useState(0);
+  const [walletHistory, setWalletHistory] = useState<MyWalletHistory[]>([]);
+  const [userWalletName, setUserWalletName] = useState('');
+
+  //내 지갑 잔액 조회 api 호출
+  useEffect(() => {
+    const fetchWalletBalance = async () => {
+      try {
+        const accessToken = '사용자_토큰_여기';
+        const response = await fetch('/api/wallets', {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${accessToken}`,
+          },
+        });
+        if (response.ok) {
+          const data: WalletDataResponse = await response.json();
+          setWalletBalance(data.amount);
+          setUserWalletName(data.userWalletName);
+          setWalletHistory(data.walletHistory);
+        } else {
+          alert('잔액을 불러오는데 실패했습니다.');
+        }
+      } catch (error) {
+        console.error('Error fetching wallet balance:', error);
+        alert('오류가 발생했습니다.');
+      }
+    };
+
+    fetchWalletBalance();
+  }, []);
 
   // 충전 API 호출 함수
   const handleCharge = async (amount: number) => {
@@ -41,14 +77,14 @@ const MyWallet: React.FC = () => {
         {/* 내 지갑 정보 */}
         <div className="w-4/5 h-1/4 bg-[#F7F6F9] rounded-lg mx-auto flex flex-col justify-between relative dark:bg-[#6C6C6C]">
           <p className="test-sm text-[clamp(0.8rem,2vw,1rem)] text-black text-start mt-4 px-4 dark:text-[#D4D4D4]">
-          {walletData.userWalletName}의 지갑
+          {userWalletName}의 지갑
           </p>
           <div className='flex flex-col items-center justify-center h-2/3'>
             <p className="text-[clamp(1rem,2.5vw,1.5rem)] sm:text-base text-black text-center dark:text-[#D4D4D4]">
               잔여 포인트
             </p>
             <p className="text-[clamp(1.5rem,4vw,2.5rem)] sm:text-3xl text-black text-center font-bold dark:text-[#D4D4D4]">
-              {walletData.availablePoint.toLocaleString()}원
+              {walletBalance.toLocaleString()}원
             </p>
           </div>
           <div className="flex justify-between w-full">
@@ -65,34 +101,36 @@ const MyWallet: React.FC = () => {
         <div className="mt-12 mx-6">
           <h3 className="text-lg text-[#969595]">입출금 내역</h3>
           <ul className="mt-2">
-            {walletData.walletHistory.map((history: MyWalletHistory) => (
-              <li
-                key={history.walletHistoryId}
-                className="py-1.5 border-b flex justify-between items-center text-lg border-gray-300 dark:border-[#515151] dark:bg-[#3b3838]"
-              >
-                {/* 날짜 & 카드 이름 */}
-                <div className="flex items-center w-1/2">
-                  <p className="text-black dark:text-white">
-                    {history.transactionDate}
-                  </p>
-                  {history.type !== 'DEPOSIT' && (
-                    <p className="text-black ml-12 dark:text-white">
-                      {history.cardName}
+            {walletHistory.length > 0 ? (walletHistory.map((history: MyWalletHistory) => (
+                <li
+                  key={history.walletHistoryId}
+                  className="py-1.5 border-b flex justify-between items-center text-lg border-gray-300 dark:border-[#515151] dark:bg-[#3b3838]"
+                >
+                  {/* 날짜 & 카드 이름 */}
+                  <div className="flex items-center w-1/2">
+                    <p className="text-black dark:text-white">
+                      {history.transactionDate}
                     </p>
-                  )}
-                </div>
-                {/* 입출금 종류 & 금액 */}
-                <div className="flex flex-col items-end">
-                  <p className={`${history.type === 'DEPOSIT' ? 'text-blue-500' : 'text-black dark:text-white'}`}>
-                    {history.type === 'DEPOSIT' ? '+' : '-'}{' '}
-                    {Math.abs(history.point).toLocaleString()}원
-                  </p>
-                  <p className="text-sm text-gray-500 dark:text-white">
-                    잔액 {history.afterPoint.toLocaleString()}원
-                  </p>
-                </div>
-              </li>
-            ))}
+                    {history.type !== 'DEPOSIT' && (
+                      <p className="text-black ml-12 dark:text-white">
+                        {history.cardName}
+                      </p>
+                    )}
+                  </div>
+                  {/* 입출금 종류 & 금액 */}
+                  <div className="flex flex-col items-end">
+                    <p className={`${history.type === 'DEPOSIT' ? 'text-blue-500' : 'text-black dark:text-white'}`}>
+                      {history.type === 'DEPOSIT' ? '+' : '-'}{' '}
+                      {Math.abs(history.point).toLocaleString()}원
+                    </p>
+                    <p className="text-sm text-gray-500 dark:text-white">
+                      잔액 {history.afterPoint.toLocaleString()}원
+                    </p>
+                  </div>
+                </li>
+              ))
+              ):(<p className="text-center text-gray-500 dark:text-white">입출금 내역이 없습니다.</p>)
+            }
           </ul>
         </div>
       </div>
