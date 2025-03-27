1f85f43a25a23b12844a910ced069836326f91eb
wmw04087 <wmw04087@gmail.com>
2025-03-28 00:29:55 +0900

    feat: 특정경로에 대해 시큐리티 필터 무시

--- backend/app/app-external-api/src/main/java/monorail/linkpay/auth/SecurityConfig.java
+++ backend/app/app-external-api/src/main/java/monorail/linkpay/auth/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -40,6 +41,12 @@ public class SecurityConfig {
                 .exceptionHandling(eh -> eh
                         .authenticationEntryPoint(defaultAuthenticationEntryPoint))
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/h2-console/**", "/favicon.ico", "/actuator/**");
     }
 
     public AuthTokenAuthenticationFilter tokenAuthenticationFilter() {
