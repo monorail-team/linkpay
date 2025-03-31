a6928de05e721593b921b787e9d110732ded9a32
강주빈 <sisoya0424@gmail.com>
2025-03-31 21:15:52 +0900

    refactor: StoreService에서 StoreSignature 생성 로직 분리

--- backend/linkpay-domain/domain-core/src/main/java/monorail/linkpay/store/service/StoreSignatureManager.java
+++ backend/linkpay-domain/domain-core/src/main/java/monorail/linkpay/store/service/StoreSignatureManager.java
@@ -1,38 +1,36 @@
 package monorail.linkpay.store.service;
 
 import lombok.RequiredArgsConstructor;
+import monorail.linkpay.annotation.SupportLayer;
 import monorail.linkpay.store.domain.Store;
 import monorail.linkpay.store.domain.StoreSignature;
-import monorail.linkpay.store.repository.StoreRepository;
 import monorail.linkpay.store.repository.StoreSignatureRepository;
 import monorail.linkpay.util.id.IdGenerator;
 import monorail.linkpay.util.key.KeyAlgorithm;
 import monorail.linkpay.util.key.KeyPairUtil;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
-@Service
+@SupportLayer
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class StoreService {
+public class StoreSignatureManager {
 
-    private final StoreRepository storeRepository;
-    private final StoreSignatureManager storeSignatureManager;
+    private final StoreSignatureRepository storeSignatureRepository;
     private final IdGenerator idGenerator;
 
     @Transactional
-    public Long create(final String name) {
-        Store store = storeRepository.save(Store.builder()
+    public void create(Store store) {
+        KeyPair keyPair = KeyPairUtil.generateKeyPair(KeyAlgorithm.RSA);
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+        storeSignatureRepository.save(StoreSignature.builder()
                 .id(idGenerator.generate())
-                .name(name)
+                .store(store)
+                .encryptKey(privateKey.getEncoded())
+                .decryptKey(publicKey.getEncoded())
                 .build());
-
-        // 기본 가게 서명 1개 생성
-        storeSignatureManager.create(store);
-        return store.getId();
     }
 }
