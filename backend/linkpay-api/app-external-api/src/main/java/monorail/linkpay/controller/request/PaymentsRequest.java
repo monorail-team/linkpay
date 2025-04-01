b8807eb19d014adb53784b659d54ea542db5874c
강주빈 <sisoya0424@gmail.com>
2025-04-01 20:21:41 +0900

    refactor: 링크카드 결제

--- backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/controller/request/PaymentsRequest.java
+++ backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/controller/request/PaymentsRequest.java
@@ -1,12 +1,32 @@
 package monorail.linkpay.controller.request;
 
 import jakarta.validation.constraints.Positive;
+import monorail.linkpay.common.domain.Point;
+import monorail.linkpay.payment.dto.PaymentInfo;
+import monorail.linkpay.payment.dto.TransactionInfo;
 
 public record PaymentsRequest(
 
         @Positive(message = "금액은 양수여야 합니다.")
         long amount,
         Long linkCardId,
-        Long storeId
+        Long storeId,
+        String transactionSignature,
+        String paymentToken
 ) {
+        public TransactionInfo txInfo() {
+                return TransactionInfo.builder()
+                        .storeId(storeId)
+                        .point(new Point(amount))
+                        .signature(transactionSignature)
+                        .build();
+        }
+
+        public PaymentInfo payInfo(Long memberId) {
+                return PaymentInfo.builder()
+                        .memberId(memberId)
+                        .linkCardId(linkCardId)
+                        .paymentToken(paymentToken)
+                        .build();
+        }
 }
