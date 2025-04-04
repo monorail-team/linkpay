3c9cc6a3b1c19e6b9686dea2d54f939b2d8cd6d5
강주빈 <sisoya0424@gmail.com>
2025-04-04 15:05:49 +0900

    style: 코드 스타일 수정

--- backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/facade/WalletHistoryFacade.java
+++ backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/facade/WalletHistoryFacade.java
@@ -6,48 +6,48 @@ import monorail.linkpay.facade.dto.WalletHistoryResponse;
 import monorail.linkpay.history.dto.WalletHistoryDto;
 import monorail.linkpay.history.dto.WalletHistoryListDto;
 import monorail.linkpay.history.service.WalletHistoryService;
-import monorail.linkpay.payment.dto.PaymentDto;
 import monorail.linkpay.payment.service.PaymentService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class WalletHistoryFacade {
 
     private final WalletHistoryService walletHistoryService;
     private final PaymentService paymentService;
 
     public WalletHistoryResponse readWalletHistory(final Long walletHistoryId, final Long memberId) {
-        WalletHistoryDto walletHistoryDto = walletHistoryService.readWalletHistory(walletHistoryId, memberId);
-        List<PaymentDto> paymentDtos =  paymentService.readPaymentsByHistoryIdIn(Set.of(walletHistoryDto.walletHistoryId()));
+        var walletHistoryDto = walletHistoryService.readWalletHistory(walletHistoryId, memberId);
+        var paymentDtos =  paymentService.readPaymentsByHistoryIdIn(Set.of(walletHistoryDto.walletHistoryId()));
         return WalletHistoryResponse.from(walletHistoryDto, paymentDtos.stream().findAny().orElse(null));
     }
 
     public WalletHistoryListResponse readLinkedWalletHistoryPage(final Long walletId,
                                                                  final Long lastId,
                                                                  final int size) {
-        WalletHistoryListDto walletHistoryListDto = walletHistoryService.readLinkedWalletHistoryPage(walletId, lastId, size);
+        var walletHistoryListDto = walletHistoryService.readLinkedWalletHistoryPage(walletId, lastId, size);
         return toWalletHistoryListResponse(walletHistoryListDto);
     }
 
     public WalletHistoryListResponse readMyWalletHistoryPage(final Long memberId,
-                                                        final Long lastId,
-                                                        final int size) {
-        WalletHistoryListDto walletHistoryListDto = walletHistoryService.readMyWalletHistoryPage(memberId, lastId, size);
+                                                             final Long lastId,
+                                                             final int size) {
+        var walletHistoryListDto = walletHistoryService.readMyWalletHistoryPage(memberId, lastId, size);
         return toWalletHistoryListResponse(walletHistoryListDto);
     }
 
-    private WalletHistoryListResponse toWalletHistoryListResponse(WalletHistoryListDto walletHistoryListDto) {
+    private WalletHistoryListResponse toWalletHistoryListResponse(final WalletHistoryListDto walletHistoryListDto) {
         var walletHistoryIds = extractWalletHistoryIds(walletHistoryListDto);
         var paymentDtos = paymentService.readPaymentsByHistoryIdIn(walletHistoryIds);
         return WalletHistoryListResponse.from(walletHistoryListDto, paymentDtos);
     }
 
-    private Set<Long> extractWalletHistoryIds(WalletHistoryListDto dto) {
+    private Set<Long> extractWalletHistoryIds(final WalletHistoryListDto dto) {
         return dto.walletHistoryDtos().stream()
                 .map(WalletHistoryDto::walletHistoryId)
                 .collect(Collectors.toUnmodifiableSet());
