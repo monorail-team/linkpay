f53d543c4e2f2dcc84fc34cbea1ee6906b2e99b5
JJBINY <sisoya0424@gmail.com>
2025-04-06 04:29:09 +0900

    feat: CORS 설정 주입

--- backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/auth/CorsConfig.java
+++ backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/auth/CorsConfig.java
@@ -1,22 +1,22 @@
 package monorail.linkpay.auth;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allow.origins}")
-    private String[] corsAllowOrigins;
+    private final CorsProps corsProps;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(corsAllowOrigins)
+                .allowedOrigins(corsProps.origins.toArray(new String[0]))
                 .allowCredentials(true)
-                .allowedMethods("*")
-                .exposedHeaders("*");
+                .allowedMethods(corsProps.methods.toArray(new String[0]))
+                .exposedHeaders(corsProps.headers.toArray(new String[0]));
     }
 }
