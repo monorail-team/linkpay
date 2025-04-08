c757792a5c5587dea353959eedae59206cd0ed71
강주빈 <sisoya0424@gmail.com>
2025-04-08 15:44:04 +0900

    test: 거래 정보 서명 응답 양식 변경 Json->Flat

--- backend/linkpay-api/app-external-api/src/test/java/monorail/linkpay/controller/ControllerFixture.java
+++ backend/linkpay-api/app-external-api/src/test/java/monorail/linkpay/controller/ControllerFixture.java
@@ -156,7 +156,7 @@ public class ControllerFixture {
             List.of(LINKED_WALLET_RESPONSE_1, LINKED_WALLET_RESPONSE_2, LINKED_WALLET_RESPONSE_3), false
     );
 
-    public static final PaymentsRequest PAYMENT_REQUEST = new PaymentsRequest(10000, "1", "2", "sig", "tkn");
+    public static final PaymentsRequest PAYMENT_REQUEST = new PaymentsRequest("1|90000|sig", "1", "tkn");
 
     public static final LinkedMemberResponse LINKED_MEMBER_RESPONSE_1 = new LinkedMemberResponse(
             "1", "링크멤버1", "link1@gmail.com");
