12255368dfe15cc093dc325d9d6ec5635d0bbc1e
강주빈 <sisoya0424@gmail.com>
2025-04-09 21:31:13 +0900

    feat: 결제 API 및 NFC 태그 연결

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import { useNavigate } from 'react-router-dom';
 import { useThemeStore } from '@/store/themeStore';
@@ -12,6 +12,10 @@ import MenuModal from '@/modal/MenuModal';
 import { Card } from '@/model/Card';
 import axios from 'axios';
 
+interface WebAuthnResponse {
+  paymentToken: string;
+}
+
 const base_url = process.env.REACT_APP_API_URL;
 
 const Home: React.FC = () => {
@@ -23,27 +27,39 @@ const Home: React.FC = () => {
 
   const { handleWebAuthn, loading } = useWebAuthn();
 
+  function extractPaymentToken(input: unknown): string | null {
+    if (
+      typeof input === 'object' &&
+      input !== null &&
+      'paymentToken' in input &&
+      typeof (input as any).paymentToken === 'string'
+    ) {
+      return (input as WebAuthnResponse).paymentToken;
+    }
+    return null; // 또는 throw Error
+  }
+
   const onFingerprintClick = async () => {
     const assertionResult = await handleWebAuthn();
     if (assertionResult) {
       const selectedCard = cards[currentIndex];
-      navigate("/payment", { state: { cardData: selectedCard} }); // TODO: state에 paymentToken 추가해주세요
+      navigate('/payment', { state: { cardData: selectedCard, paymentToken: extractPaymentToken(assertionResult) } });
     } else {
-      console.error("assertionResult 생성 실패");
+      console.error('assertionResult 생성 실패');
     }
   };
 
   const [showMenu, setShowMenu] = useState(false);
-  
+
   const handleMenuClick = () => {
     setShowMenu(true);
   };
-  
+
   const handleMenuClose = () => {
     setShowMenu(false);
   };
 
- 
+
   // 카드 API 호출
   useEffect(() => {
     const fetchCards = async () => {
@@ -54,7 +70,7 @@ const Home: React.FC = () => {
           return;
         }
         const response = await axios.get(`${base_url}/api/cards/registered`, {
-          headers: { Authorization: `Bearer ${token}` },
+          headers: { Authorization: `Bearer ${token}` }
         });
         const cardsData = response.data.linkCards as Card[];
         setCards(cardsData);
@@ -66,8 +82,8 @@ const Home: React.FC = () => {
   }, []);
 
 
-   // 지갑 API 호출
-   useEffect(() => {
+  // 지갑 API 호출
+  useEffect(() => {
     const fetchWallet = async () => {
       try {
         const token = sessionStorage.getItem('accessToken');
@@ -76,7 +92,7 @@ const Home: React.FC = () => {
           return;
         }
         const response = await axios.get(`${base_url}/api/my-wallets`, {
-          headers: { Authorization: `Bearer ${token}` },
+          headers: { Authorization: `Bearer ${token}` }
         });
         setWalletInfo(response.data);
       } catch (error) {
@@ -97,7 +113,7 @@ const Home: React.FC = () => {
     onSwipedRight: () => {
       if (currentIndex > 0) setCurrentIndex(currentIndex - 1);
     },
-    trackMouse: true,
+    trackMouse: true
   });
 
   // 카드 위치 계산
@@ -106,33 +122,31 @@ const Home: React.FC = () => {
   const translatePercent = containerPositionPercent - currentIndex * cardTotalWidthPercent;
 
 
-
-
   // 플러스 카드 클릭 시 호출
   const handleAddLinkClick = () => {
     navigate('/register');
   };
 
   return (
-    <div className='dark:bg-[#3b3838] overflow-hidden h-screen w-full'>
-    <Header headerType="menu" onMenuClick={handleMenuClick} />
+    <div className="dark:bg-[#3b3838] overflow-hidden h-screen w-full">
+      <Header headerType="menu" onMenuClick={handleMenuClick} />
       {showMenu && <MenuModal onClose={handleMenuClose} />}
 
-    <div className="relative w-4/5 max:w-[456px] mx-auto  mt-10 rounded-lg">
-      <div className='p-4 
+      <div className="relative w-4/5 max:w-[456px] mx-auto  mt-10 rounded-lg">
+        <div className="p-4
             bg-white dark:bg-[#5a5757]
             border border-gray-200 dark:border-gray-600
-            rounded-lg'>
-        {/* 카드 영역 */}
+            rounded-lg">
+          {/* 카드 영역 */}
           <div
             {...handlers}
             className="flex transition-transform duration-300 mt-[90px]"
             style={{ transform: `translateX(${translatePercent}%)` }}
-          > 
+          >
             {/* 실제 카드들을 화면에 표시 */}
             {cards.map((card) => (
-              <div 
-                key={card.linkCardId} 
+              <div
+                key={card.linkCardId}
                 className="flex-shrink-0 mr-[10%] last:mr-0 transition-opacity duration-300 w-full"
                 style={{ opacity: Math.abs(currentIndex - cards.indexOf(card)) > 1 ? 0.5 : 1 }}
               >
@@ -145,30 +159,32 @@ const Home: React.FC = () => {
               className="flex-shrink-0 mr-[3.5%] last:mr-0 transition-opacity duration-300 w-full"
               style={{ opacity: currentIndex === cards.length ? 1 : 0.5 }}
             >
-              <AddLinkCard onClick={handleAddLinkClick}/>
+              <AddLinkCard onClick={handleAddLinkClick} />
             </div>
           </div>
-            {/* Link Pay 문구 */}
-            <div className="text-center text-lg text-[#969292] font-bold mt-3">{currentIndex !== cards.length?"Link Card":"링크카드 등록"}</div>
+          {/* Link Pay 문구 */}
+          <div
+            className="text-center text-lg text-[#969292] font-bold mt-3">{currentIndex !== cards.length ? 'Link Card' : '링크카드 등록'}</div>
 
-            {/* my link 섹션 */}
-            <div className="flex flex-col flex-1 items-center justify-center min-h-[80px]">
-              {walletInfo?.amount !== undefined && currentIndex !== cards.length?(
+          {/* my link 섹션 */}
+          <div className="flex flex-col flex-1 items-center justify-center min-h-[80px]">
+            {walletInfo?.amount !== undefined && currentIndex !== cards.length ? (
                 <div className="text-2xl font-medium mb-2 text-center dark:text-white">
                   지갑 잔액 {walletInfo.amount.toLocaleString()} 원
                 </div>
-              ) : 
+              ) :
               (
-              // 다른 사람의 지갑인 경우
+                // 다른 사람의 지갑인 경우
                 <div className="h-8"></div>
               )}
-            </div>
           </div>
+        </div>
       </div>
-      
+
 
       {/* 지문 아이콘 및 결제 문구 */}
-      <footer className="absolute  flex flex-col text-center items-center bottom-[30px] left-[50%] -translate-x-[50%] w-full">
+      <footer
+        className="absolute  flex flex-col text-center items-center bottom-[30px] left-[50%] -translate-x-[50%] w-full">
         <button
           onClick={onFingerprintClick}
           disabled={loading}
