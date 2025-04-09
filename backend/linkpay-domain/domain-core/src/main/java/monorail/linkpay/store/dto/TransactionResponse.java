997e8aba1e2ae7e689a13cd7bafd6da118306dd9
강주빈 <sisoya0424@gmail.com>
2025-04-09 21:30:49 +0900

    feat: NFC 카드 데이터 크기(99bytes)에 맞게 데이터 리사이징

--- backend/linkpay-domain/domain-core/src/main/java/monorail/linkpay/store/dto/TransactionResponse.java
+++ backend/linkpay-domain/domain-core/src/main/java/monorail/linkpay/store/dto/TransactionResponse.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import monorail.linkpay.payment.dto.TransactionInfo;
 import monorail.linkpay.util.encoder.FlatEncoder;
 
+import java.nio.charset.StandardCharsets;
+
 @Builder
 public record TransactionResponse(
         String storeId,
@@ -18,9 +20,20 @@ public record TransactionResponse(
     }
 
     public Flat flat() {
-        return new Flat(FlatEncoder.encode(this));
+        Flat.Dto target = new Flat.Dto(Long.parseLong(storeId), amount, transactionSignature);
+        String flatString = FlatEncoder.encode(target);
+        System.out.println("flatString = " + flatString);
+        Flat flat = new Flat(flatString.replace(FlatEncoder.DELIMITER, ""));
+        byte[] bytes = flat.data.getBytes(StandardCharsets.UTF_8);
+        System.out.println("flat.data = " + flat.data);
+        System.out.println("📦 Length: " + bytes.length + " bytes");
+        return flat;
     }
 
+
+
     public record Flat(String data) {
+        private record Dto(long storeId, long amount, String signature) {
+        }
     }
 }
