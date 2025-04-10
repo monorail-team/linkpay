c64e80d7daf4511b89e2f264c4b1bef92546cd63
최성환 <aiya0422@gmail.com>
2025-04-10 10:45:17 +0900

    refactor: 불필요한 TossService 클래스 제거

--- backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/controller/ThirdPartyController.java
+++ backend/linkpay-api/app-external-api/src/main/java/monorail/linkpay/controller/ThirdPartyController.java
@@ -2,7 +2,7 @@ package monorail.linkpay.controller;
 
 import lombok.RequiredArgsConstructor;
 import monorail.linkpay.controller.request.PaymentConfirmRequest;
-import monorail.linkpay.thirdparty.toss.TossService;
+import monorail.linkpay.thirdparty.toss.TossClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/toss")
+@RequestMapping("/api/thirdparty")
 @RequiredArgsConstructor
-public class TossController {
+public class ThirdPartyController {
 
-    private final TossService tossService;
+    private final TossClient tossClient;
 
-    @PostMapping("/checkout/confirm")
-    public ResponseEntity<?> confirmPayment(@RequestBody final PaymentConfirmRequest request) {
-        tossService.confirmPayment(request.paymentKey(), request.amount(), request.orderId());
-        return ResponseEntity.ok().build();
+    @PostMapping("/toss/checkout/confirm")
+    public ResponseEntity<Void> confirmPayment(@RequestBody final PaymentConfirmRequest request) {
+        tossClient.confirmPayment(request.paymentKey(), request.amount(), request.orderId());
+        return ResponseEntity.noContent().build();
     }
 }
--- backend/linkpay-domain/domain-core/src/main/java/monorail/linkpay/thirdparty/toss/TossClient.java
+++ backend/linkpay-domain/domain-core/src/main/java/monorail/linkpay/thirdparty/toss/TossClient.java
@@ -4,27 +4,32 @@ import lombok.RequiredArgsConstructor;
 import monorail.linkpay.exception.LinkPayException;
 import monorail.linkpay.thirdparty.toss.dto.TossConfirmRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static monorail.linkpay.exception.ExceptionCode.SERVER_ERROR;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class TossClient {
+
+    public static final String TOSS_PAYMENTS_CONFIRM = "https://api.tosspayments.com/v1/payments/confirm";
 
     @Value("${toss.secret-key}")
     private String secretKey;
 
     private final RestTemplate restTemplate;
 
-    public void confirmPayment(String paymentKey, Long amount, String orderId) {
-        String credentials = Base64.getEncoder().encodeToString((secretKey + ":").getBytes(StandardCharsets.UTF_8));
+    public void confirmPayment(final String paymentKey, final Long amount, final String orderId) {
+        String credentials = Base64.getEncoder().encodeToString((secretKey + ":").getBytes(UTF_8));
         TossConfirmRequest requestBody = new TossConfirmRequest(paymentKey, orderId, amount);
 
         HttpHeaders headers = new HttpHeaders();
@@ -33,7 +38,7 @@ public class TossClient {
         HttpEntity<TossConfirmRequest> httpEntity = new HttpEntity<>(requestBody, headers);
 
         ResponseEntity<Object> response = restTemplate.exchange(
-                "https://api.tosspayments.com/v1/payments/confirm",
+                TOSS_PAYMENTS_CONFIRM,
                 HttpMethod.POST,
                 httpEntity,
                 Object.class
